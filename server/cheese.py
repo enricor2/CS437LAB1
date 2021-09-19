d73dea44a3cf638df4288a0bdb2813b5c96f54c6
enricor2 <enricor2@illinois.edu>
2021-09-19 18:25:33 -0500

    updates

--- server/cheese.py
+++ server/cheese.py
@@ -3,11 +3,12 @@ import rotation
 import move
 import keyboard
 if __name__ == '__main__':
-    if keyboard.is_pressed('w'):
-        moveForward.move1()
-    elif keyboard.is_pressed('a'):
-        rotation.rotateDegLeft()
-    elif keyboard.is_pressed('d'):
-        rotation.rotateDegRight()
+    while(True):
+        if keyboard.is_pressed('w'):
+            moveForward.move1()
+        elif keyboard.is_pressed('a'):
+            rotation.rotateDegLeft()
+        elif keyboard.is_pressed('d'):
+            rotation.rotateDegRight()
     
 
