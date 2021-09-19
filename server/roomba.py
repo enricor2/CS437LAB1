40ebb5690e3e3065d40eaded2c080fd12655dcbe
enricor2 <enricor2@illinois.edu>
2021-09-18 20:29:49 -0500

    updates

--- server/roomba.py
+++ server/roomba.py
@@ -4,22 +4,38 @@ import time
 import numpy as np
 move.setup()
 
+rotationTime = .0165
+
+def getRotationTime():
+    global rotationTime
+    for x in range(360):
+        rotateDegRight()
+    print("Enter degrees rotated: ")
+    rotation = float(input())
+    rotationTime = rotationTime * 360 / rotation
+    if rotation > 360:
+        for x in range(int(rotation)-360):
+            rotateDegLeft()
+    elif rotation < 360:
+        for x in range(360-int(rotation)):
+            rotateDegRight()
+
 def rotateDegRight():
         move.move(100,'no','right',0.8)
-        time.sleep(.0181)
+        time.sleep(rotationTime)
         move.motorStop()
         time.sleep(.05)
 
 def rotateDegLeft():
         move.move(100,'no','left',0.8)
-        time.sleep(.01935)
+        time.sleep(rotationTime)
         move.motorStop()
         time.sleep(.05)
 
 def roomba():
         if ultra.checkdist() < .1:
-                move.move(100, 'backward', 'no', .5)
-                time.sleep(1.5)
+                move.move(80, 'backward', 'no', .5)
+                time.sleep(.75)
                 if np.random.rand() > .5:
                         for x in range(90):
                                 rotateDegRight()
@@ -29,7 +45,7 @@ def roomba():
                                 rotateDegLeft()
                                 x+=1
         else:
-                move.move(100,'forward', 'no', .5)
+                move.move(80,'forward', 'no', .5)
         time.sleep(.1)
 
 
