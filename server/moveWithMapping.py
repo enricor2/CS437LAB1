13b9039c098f805eefe97da37f3c9945e5de18a3
enricor2 <enricor2@illinois.edu>
2021-09-19 10:02:52 -0500

    updates

--- server/moveWithMapping.py
+++ server/moveWithMapping.py
@@ -8,6 +8,7 @@ move.setup()
 np.set_printoptions(threshold=sys.maxsize)
 rotationTime = .0165
 travelTime = .25
+dir = 1 # 1-U 2-D 3-R 4-L
 
 def getRotationTime():
     global rotationTime
@@ -103,12 +104,106 @@ def plotLine(x0, y0, x1, y1,grid,x_loc,y_loc):
             err += dx
             y0 += sy 
 
+def move2():
+    global travelTime
+    move.move(50,'forward','no',0.8)
+    time.sleep(travelTime)
+    move.motorStop()
+
+def followPath(path,x_loc, y_loc, dir,numStep = 400):
+    for x in range (numStep):
+        if path[x] == path[-1]:
+            print("Arrived at end of path.")
+            return x_loc, y_loc
+        nextStep = path[x+1]-path[x]    
+        turn(dir,nextStep)
+        for x in range(4):
+            move2()
+
+def turn(dir, nextStep):
+    if dir == 1:                    #F
+        if nextStep == [1,0]:       #F
+            return
+        elif nextStep == [0,1]:     #R
+            for x in range(90):
+                rotateDegRight()
+            dir = 3
+            return
+        elif nextStep == [-1,0]:    #B
+            for x in range(180):
+                rotateDegRight()
+            dir = 2
+            return
+        elif nextStep == [0,-1]:    #L
+            for x in range(90):
+                rotateDegLeft()
+            dir = 4
+            return
+    elif dir == 2:                  #B
+        if nextStep == [1,0]:       #F
+            for x in range(180):
+                rotateDegRight()
+            dir = 1
+            return
+        elif nextStep == [0,1]:     #R
+            for x in range(90):
+                rotateDegLeft()
+            dir = 3
+            return
+        elif nextStep == [-1,0]:    #B
+            return
+        elif nextStep == [0,-1]:    #L
+            for x in range(90):
+                rotateDegRight()
+            dir = 4
+            return
+    elif dir == 3:                  #R
+        if nextStep == [1,0]:       #F
+            for x in range(90):
+                rotateDegLeft()
+            dir = 1
+            return
+        elif nextStep == [0,1]:     #R
+            return
+        elif nextStep == [-1,0]:    #B
+            for x in range(90):
+                rotateDegRight()
+            dir = 2
+            return
+        elif nextStep == [0,-1]:    #L
+            for x in range(180):
+                rotateDegRight()
+            dir = 4
+            return
+    elif dir == 4:
+        if nextStep == [1,0]:       #F
+            for x in range(90):
+                rotateDegRight()
+            dir = 1
+            return
+        elif nextStep == [0,1]:     #R
+            for x in range(180):
+                rotateDegRight()
+            dir = 3
+            return
+        elif nextStep == [-1,0]:    #B
+            for x in range(90):
+                rotateDegLeft()
+            dir = 2
+            return
+        elif nextStep == [0,-1]:    #L
+            return
+
+
+
 if __name__ == '__main__':
     try:
+        x_loc = 0
+        y_loc = 0
         print("X position to travel to in m: ")
-        x = float(input())
+        x = int(float(input())/.08)
         print("y position to travel to in m: ")
-        y = float(input())
+        y = int(float(input())/.08)
         print("Please wait for rotation testing...")
         getRotationTime()
         print("Please wait for distance testing...")
@@ -118,7 +213,13 @@ if __name__ == '__main__':
         grid = np.full((100,100),0)
         createMapping(0,0,grid)
         print("Planning a path")
-        path = aStar.relativeListPath(aStar.aStar(grid,(0,0),(x/8,y/8),50,50))
+        path = aStar.relativeListPath(aStar.aStar(grid,(49,49),(49-x,y-49),49,49))
+        while (x_loc != x and y_loc != y):
+            if path[-1] != (x,y):
+                x_loc, y_loc = followPath(path,x_loc,dir,6)
+            else:
+                x_loc,y_loc = followPath(path,x_loc,dir)
+
         print(path)
         print("Beginning travel...")
         
